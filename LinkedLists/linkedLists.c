35aa8416488b6e4634350f38452f891422c2e285
arixx02 <ar.agustin.bt02@gmail.com>
2025-05-01 21:31:15 -0300

    renaming

--- LinkedLists/linkedLists.c
+++ LinkedLists/linkedLists.c
@@ -78,7 +78,7 @@ void printList(List *list){
 
 int listLength(const Node *head){
     int length=0;
-    Node* current=head;
+    const Node* current=head;
     while(current!=NULL){
         length++;
         current=current->next;
